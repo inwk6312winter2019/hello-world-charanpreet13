f28a871a7a984f7b74dda3b1d237d12a7a0006c4
charanpreet13 <c.oberoi@dal.ca>
2019-02-04 15:27:45 +0000

    corrections

--- classroomtask3.py
+++ classroomtask3.py
@@ -1,12 +1,13 @@
-class card()
+class card():
+
 	suite_names=['clubs','diamonds','hearts','spades']
-	rank_names=[none,'ace','2','3','4','5','6','7','8','9','10','jack','queen','king']
-	def __init__(self,rank=0,suite=2)
+	rank_names=[None,'ace','2','3','4','5','6','7','8','9','10','jack','queen','king']
+	def __init__(self,rank=0,suite=2):
 		self.rank=rank
 		self.suite=suite
 	
 
-	def __init__(self):
-		return f"the rank is {card.rank_names[self.rank]} and the suite is {card..suite_names[self.suite]}
-ace_of_spade=card(1,3)
+	def __str__(self):
+		return f"the rank is {card.rank_names[self.rank]} and the suite is {card.suite_names[self.suite]}"
+ace_of_spade=card()
 print(ace_of_spade)
